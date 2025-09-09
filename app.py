705e7d14c6288e164fef0b88bd87e9d4a89795dd
sara-jabeennn <syedajabean@gmail.com>
2025-09-10 01:25:43 +0500

    Fix syntax error for deployment

--- app.py
+++ app.py
@@ -13,8 +13,5 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
 @app.route("/broken")
-def broken()
-    return "This will break"
-@app.route("/broken")
-def broken()  # <-- missing colon here
-    return "This will break GitHub Actions!"
+def broken():  # <-- fixed
+    return "This works now!"
