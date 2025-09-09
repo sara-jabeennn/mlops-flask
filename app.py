4fabc57052325543a44f7fdcbda77f3bb19c8b04
sara-jabeennn <syedajabean@gmail.com>
2025-09-09 23:32:03 +0500

    Update /hello route for feature branch

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def home():
     return "Hello, MLOps on Azure!"
 
+@app.route("/hello")
+def hello():
+    return "Hello from Azure!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
