cca668076dc19c6173fefcddc9a21bedb42bbee9
sara-jabeennn <syedajabean@gmail.com>
2025-09-10 02:04:33 +0500

    Fix flake8 style issues

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Hello, MLOps on Azure!"
 
+
 @app.route("/hello")
 def hello():
     return "Hello from Azure!"
+
 
 @app.route("/broken")
 def broken():  # fixed
     return "This works now!"
 
+
 def broken_style():
     return "Extra spaces fixed"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
